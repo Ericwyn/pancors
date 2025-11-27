4f288bb76adda6f1c4817b829c66bcd166c6adc2
ericwyn <16485562+Ericwyn@users.noreply.github.com>
2025-11-27 11:47:45 +0800

    update cmd

--- cmd/pancors/main.go
+++ cmd/pancors/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -22,17 +24,42 @@ func getAllowCredentials() string {
 	return "true"
 }
 
-func getListenPort() string {
+func getListenPort(portFlag string) string {
+	// Command line flag takes precedence over environment variable
+	if portFlag != "" {
+		// Ensure port starts with colon
+		if portFlag[0] != ':' {
+			return ":" + portFlag
+		}
+		return portFlag
+	}
+
 	if port, ok := os.LookupEnv("PORT"); ok {
 		return ":" + port
 	}
 	return ":8080"
 }
 
+const version = "0.1.0"
+
 func main() {
+	// Define command line flags
+	var showVersion bool
+	var portFlag string
+
+	flag.BoolVar(&showVersion, "v", false, "Show version information")
+	flag.StringVar(&portFlag, "port", "", "Port to listen on (overrides PORT environment variable)")
+	flag.Parse()
+
+	// Handle version flag
+	if showVersion {
+		fmt.Printf("PanCORS version %s\n", version)
+		return
+	}
+
 	http.HandleFunc("/", pancors.HandleProxyWith(getAllowOrigin(), getAllowCredentials()))
 
-	port := getListenPort()
+	port := getListenPort(portFlag)
 	log.Printf("PanCORS started listening on %s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
